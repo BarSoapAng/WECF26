d5257537d5bc7bf22dfd00b5821720441b7b0625
Angela <90152457+BarSoapAng@users.noreply.github.com>
2026-08-26 23:08:34 -0700

    Initial commit

---
+++ src/components/base/Layout/SectionWrapper.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  margin: 0 auto;
+  margin-top: -1px;
+  width: 100%;
+  position: relative;
+`;
+
+type Props = ComponentPropsWithoutRef<"section"> & {
+  id?: string;
+};
+
+const SectionWrapper = forwardRef<HTMLElement, Props>(
+  ({ id, children, ...props }, ref) => (
+    <StyledSection id={id} ref={ref} {...props}>
+      {children}
+    </StyledSection>
+  )
+);
+
+export default SectionWrapper;
